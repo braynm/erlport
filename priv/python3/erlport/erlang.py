0c9deb2ce14672a3ca514709d54bba704b8ff565
Bry Madrid <hi@braynm.com>
2025-09-16 15:37:28 +0800

    fix(python3): fix inspect compatibility with python3

--- priv/python3/erlport/erlang.py
+++ priv/python3/erlport/erlang.py
@@ -25,7 +25,8 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from inspect import getfullargspec
+import inspect
+
 import sys
 from sys import exc_info
 from traceback import extract_tb, format_list
@@ -34,6 +35,7 @@ import uuid
 
 from erlport import Atom
 
+getargspec = inspect.getfullargspec
 
 class Error(Exception):
     """ErlPort Error."""
